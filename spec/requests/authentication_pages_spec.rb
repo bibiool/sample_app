cec9ec9d691b10a001e9b7eda4a159bf3e2ffaf4
zerusye <wjtjt839@yahoo.co.jp>
2015-06-14 15:18:02 +0000

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -59,6 +59,18 @@ describe "Authentication" do
           click_button "Sign in"
         end
         
+        describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+        
         describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -93,7 +105,17 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
-    end
+    
+    describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
     
     describe "visiting the user index" do
           before { visit users_path }
